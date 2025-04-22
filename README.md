d0a88789c890eeeb182a4210ca6334a177d25d0d
xijing21 <sinjonxi@163.com>
2025-04-22 15:52:45 +0800

    add core and device plugins

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# ruyisdk-eclipse-plugins
-RuyiSDK Eclipse Plugins
+# custom-plugins
+
+## 目录结构说明
+
+```
+├── features
+├── plugins
+│   ├── org.ruyisdk.core
+│   ├── org.ruyisdk.devices
+│   ├── org.ruyisdk.news
+│   ├── org.ruyisdk.packages
+│   ├── org.ruyisdk.ruyi
+│   └── org.ruyisdk.ui
+└── README.md
+
+```
+
+* org.ruyisdk.core:基础类，供其它插件调用的公共部分
+* org.ruyisdk.devices:RISC-V设备管理
+
---
+++ plugins/org.ruyisdk.devices/src/org/ruyisdk/devices/views/DeviceDialog.java
@@ -0,0 +1,81 @@
+package org.ruyisdk.devices.views;
+
+import org.eclipse.jface.dialogs.TitleAreaDialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.*;
+import org.ruyisdk.devices.model.Device;
+
+public class DeviceDialog extends TitleAreaDialog {
+    private Device device;
+    private String titleText;
+    private Text nameText;
+    private Text chipText;
+    private Text vendorText;
+    private Text versionText;
+    
+    public DeviceDialog(Shell parentShell, Device device) {
+        super(parentShell);
+        this.device = device;
+        this.titleText = device == null ? "Add New Device" : "Edit Device";
+    }
+    
+    @Override
+    protected void configureShell(Shell shell) {
+        super.configureShell(shell);
+        shell.setText("Device Manage"); // 这是窗口标题栏文字（不是对话框内容区的标题）
+    }
+    
+    
+    @Override
+    protected Control createDialogArea(Composite parent) {
+    	setTitle(this.titleText );
+        Composite area = (Composite) super.createDialogArea(parent);
+        Composite container = new Composite(area, SWT.NONE);
+        container.setLayout(new GridLayout(2, false));
+        container.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+
+        new Label(container, SWT.NONE).setText("Name:");
+        nameText = new Text(container, SWT.BORDER);
+        nameText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+
+        new Label(container, SWT.NONE).setText("Chip:");
+        chipText = new Text(container, SWT.BORDER);
+        chipText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+
+        new Label(container, SWT.NONE).setText("Vendor:");
+        vendorText = new Text(container, SWT.BORDER);
+        vendorText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+
+        new Label(container, SWT.NONE).setText("Version:");
+        versionText = new Text(container, SWT.BORDER);
+        versionText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+
+        if (device != null) {
+            nameText.setText(device.getName());
+            chipText.setText(device.getChip());
+            vendorText.setText(device.getVendor());
+            versionText.setText(device.getVersion());
+        }
+
+        return area;
+    }
+    
+    
+    @Override
+    protected void okPressed() {
+    	device = new Device(
+            nameText.getText(),
+            chipText.getText(),
+            vendorText.getText(),
+            versionText.getText(),
+            false
+        );
+        super.okPressed();
+    }
+
+    public Device getDevice() {
+        return device;
+    }
+}
